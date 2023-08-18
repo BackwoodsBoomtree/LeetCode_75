0d9b87b1c23156b5bc1bafb530b9c31d29a72185
Russell Doughty <rustymonroe@gmail.com>
2023-08-17 19:40:43 -0500

    Update

---
+++ CanPlaceFlowers/Program.cs
@@ -0,0 +1,51 @@
+﻿public class Solution
+{
+    public bool CanPlaceFlowers(int[] flowerbed, int n)
+    {
+        int newFlowerCounter = 0;
+
+        if (flowerbed.Length == 1 && flowerbed[0] == 0)
+            {
+            return true;
+            }
+
+        for (int i = 0; i < flowerbed.Length - 1; i++)
+        {
+            // If two open spots at beginning
+            if (
+                i == 0 && 
+                flowerbed[i] == 0 && 
+                flowerbed[i + 1] == 0)
+            {
+                newFlowerCounter += 1;
+            }
+
+            // If two open spots at the end
+            else if (i == flowerbed.Length - 2)
+            {
+                if (
+                    flowerbed[i] == 0 && 
+                    flowerbed[i + 1] == 0)
+                {
+                    newFlowerCounter += 1;
+                }
+            }
+
+            // If three open spots in a row
+            else if (
+                i < flowerbed.Length - 2 && 
+                flowerbed[i] == 0 &&
+                flowerbed[i + 1] == 0 &&
+                flowerbed[i + 2] == 0)
+            {
+                newFlowerCounter += 1;
+                i += 1;
+            }
+        }
+        if (n <= newFlowerCounter)
+        {
+            return true;
+        }
+        else { return false; }
+    }
+}
---
+++ ReverseVowelsOfAString/Program.cs
@@ -0,0 +1,61 @@
+﻿Solution test = new Solution();
+var testResult = test.ReverseVowels("racecar");
+
+Console.WriteLine(testResult);
+Console.ReadKey();
+
+public class Solution
+{
+    public string ReverseVowels(string s)
+    {
+        if (!string.IsNullOrWhiteSpace(s))
+        {
+            string result = "";
+            string vowelsInString = "";
+            List<int> vowelsIndex = new();
+            int vowelCounter = 0;
+
+            // Identify vowels and their location
+            for (int i = 0; i < s.Length; i++)
+            {
+                if ("aeiouAEIOU".IndexOf(s[i]) >= 0)
+                {
+                    vowelsInString += s[i];
+                    vowelsIndex.Add(i);
+                }
+            }
+
+            if (vowelsInString.Length <= 0)
+            {
+                return s;
+            }
+
+            else if (vowelsInString.Length > 1)
+            {
+                // Insert back into string in reverse order
+                string reverseVowels = new(vowelsInString.Reverse().ToArray());
+                for (int i = 0; i < s.Length; ++i)
+                {
+                    if (vowelCounter < vowelsInString.Length)
+                    {
+                        if (vowelsIndex[vowelCounter] == i)
+                        {
+                            result += reverseVowels[vowelCounter];
+                            vowelCounter++;
+                        }
+                        else
+                        {
+                            result += s[i];
+                        }
+                    }
+                    else
+                    {
+                        result += s[i];
+                    }
+                }
+                return result;
+            }
+        }
+        return s;
+    }
+}
