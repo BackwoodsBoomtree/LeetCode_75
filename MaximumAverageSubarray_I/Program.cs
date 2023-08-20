779c1a11273c4921d137b24940b81f9cde6527c5
Russell Doughty <rustymonroe@gmail.com>
2023-08-20 11:50:44 -0500

    Working

---
+++ MaximumAverageSubarray_I/Program.cs
@@ -0,0 +1,36 @@
+﻿var test = new Solution();
+
+int[] myArray = { 1, 12, -5, -6, 50, 3 };
+int myK = 4;
+var testResult = test.FindMaxAverage(myArray, myK);
+
+Console.WriteLine(testResult);
+Console.ReadKey();
+
+public class Solution
+{
+    public double FindMaxAverage(int[] nums, int k)
+    {
+        double[] doubleNums = new double[nums.Length];
+        List<double> doubleList = new List<double>();
+
+        // Cast array of int to list of doubles
+        for (int i = 0; i < nums.Length; i++)
+        {
+            doubleNums[i] = Convert.ToDouble(nums[i]);
+        }
+
+        if (nums.Length == 1)
+        {
+            return doubleNums[0];
+        }
+
+        for (int i  = 0; i < doubleNums.Length - k + 1; i++)
+        {
+            double loopAverage = doubleNums[i..(i + k )].Average();
+            doubleList.Add(loopAverage);
+        }
+
+        return doubleList.Max();
+    }
+}
